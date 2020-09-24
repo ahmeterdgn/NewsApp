e60a2c3ac40e971b43e7fde6277a605b92f00212
Ahmet Erdoğan <48730205+ahmeterdgn@users.noreply.github.com>
2020-09-24 11:16:32 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/ahmeterdgn/newsapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ahmeterdgn.newsapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/screens/home.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(routes: {
+      '/': (context) => HomePage(),
+    });
+  }
+}
